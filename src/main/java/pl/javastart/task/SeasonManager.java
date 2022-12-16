70ad79ff7bc08033ce7ef0cbc6eb2b80afc7a7bc
Betina Josefowicz <betina.josefowicz@gmail.com>
2022-12-16 19:41:54 +0100

    dodanie rozwiazania

--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,12 +1,22 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
+        System.out.println("Podaj porę roku:");
+        for (Season value : Season.values()) {
+            System.out.println(value.getTranslation());
+        }
 
+        String unserInput = scanner.nextLine();
+        Season userSeason = Season.getSeasonByTranslation(unserInput);
+
+        System.out.println("W tej porze roku są następujące miesiące:");
+        System.out.println(Arrays.toString(userSeason.getMonths()));
     }
 
 }
